9c66f275033a9956b66d361448aab31cd8ced1de
Serhii Klymets <ksd2000@ukr.net>
2023-08-12 23:27:23 +0300

    дз

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,6 +1,5 @@
 const fs = require("fs/promises");
 const path = require("path");
-
 const { Users } = require("../../models/users");
 
 const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
